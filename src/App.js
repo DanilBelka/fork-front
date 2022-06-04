f1d1b6e438dd980bc39ccbeef3b0981d97d9ce24
Danil <danil.belikov.y.gd@gmail.com>
2022-06-04 19:55:47 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, {useState} from "react";
 import TicketArea from "./TicketArea";
 
 function App() {
-	let [someCounter, setSomeCounter] = useState(0)
-
 	return (
 		<div className="App">
 			<div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-around"}}>
