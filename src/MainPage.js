7f6856fb8dd0f0701855a9057da17b53d2512baa
Ilya Sereda <ilyasereda121@gmail.com>
2022-07-07 18:58:34 +0300

    Finished MainPage Layout

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import vector from "./images/Vector.png"
 
 import './styles/stylesheet.css';
 import Header from "./Header"
@@ -7,8 +8,15 @@ import Footer from "./Footer"
 const MainPage = () => {
 
     return (
-        <div className={"MainPage"}>
+        <div className={"mainPage"}>
             <Header></Header>
+            <div className={"main"} style={{ marginLeft: "auto", marginRight: "auto" }}>
+                <div>
+                    <span className={"main__text"}>Welcome to FORK - start your own path.</span>
+                    <button className={"main__create-button"}>Create project</button>
+                </div>
+                <div style={{backgroundImage: `url(${vector})`, backgroundSize:"100% 100%"}}></div>
+            </div>
             <Footer></Footer>
         </div>
     );
