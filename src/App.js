8fbb0671b7612b0a3b0e1dab9d6a6e81c6ffffe7
Ilya Sereda <ilyasereda121@gmail.com>
2022-07-08 02:18:57 +0300

    Finished TaskCreatePage Layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import WelcomePage from "./WelcomePage";
 import MainPage from "./MainPage";
 import NewProjectPage from "./NewProjectPage";
+import TaskCreation from "./NewTaskPage"
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NotFound from "./NotFound";
 
@@ -11,6 +12,7 @@ function App() {
 				<Route path={"/welcome"} element={<WelcomePage/>}></Route>
 				<Route path={"/index"} element={<MainPage/>}></Route>
 				<Route path={"/project-creation"} element={<NewProjectPage/>}></Route>
+				<Route path={"/task-creation"} element={<TaskCreation/>}></Route>
 				<Route path={"*"} element={<NotFound/>}/>
 			</Routes>
 		</BrowserRouter>
